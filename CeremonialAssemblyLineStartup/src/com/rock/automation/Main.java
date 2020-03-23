4dc3aa55846de94454cd3dae64302dea1b6c4e57
Wojciech Mamys <wojciech.mamys@cognifide.com>
2020-03-23 11:02:53 +0100

    Adding varibales for eployees and plcs number

--- CeremonialAssemblyLineStartup/src/com/rock/automation/Main.java
+++ CeremonialAssemblyLineStartup/src/com/rock/automation/Main.java
@@ -6,27 +6,33 @@ public class Main {
  * PROGRAM mode is indicated as "1"
  */
     public static void main(String[] args) {
-        int counter = 0;
+        int plcsNumber = 1100;
+        int employeesNumber = 1100;
+        int runModeCounter = 0;
+        int programModeCounter = 0;
         int[] anArray;
-        anArray = new int[1100];
-        for(int i=1;i<=1100;i++)
+        anArray = new int[plcsNumber];
+
+        for(int i=1;i<=employeesNumber;i++)
         {
             if (i==1) {
-                for(int j=1;j<=1100;j++) {
+                for(int j=1;j<=plcsNumber;j++) {
                     anArray[j-1]=0;
                 }
             } else {
-                for(int j=1;j<=1100;j=j+i) {
+                for(int j=1;j<=plcsNumber;j=j+i) {
                     if (anArray[j-1]==0) anArray[j-1]=1;
                     else if (anArray[j-1]==1) anArray[j-1]=0;
                 }
             }
         }
+        
         for(int i=1;i<=1100;i++) {
-            if (anArray[i-1]==0) counter++;
+            if (anArray[i-1]==0) runModeCounter++;
+            else programModeCounter++;
             System.out.println("Element at index: " + i + " is " + anArray[i-1]);
-//            System.out.println("There are " + counter + " PLCs in RUN mode.");
         }
-        System.out.println("There are " + counter + " PLCs in RUN mode.");
+        System.out.println("There are " + runModeCounter + " PLCs in RUN mode.");
+        System.out.println("There are " + programModeCounter + " PLCs in PROGRAM mode.");
     }
 }
