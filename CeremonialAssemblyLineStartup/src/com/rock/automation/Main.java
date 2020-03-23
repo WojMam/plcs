ad38ab5dce5fb4a9afcf8721a6aeb5cdb06f6553
Wojciech Mamys <wojciech.mamys@cognifide.com>
2020-03-23 11:14:48 +0100

    Changing variables names

--- CeremonialAssemblyLineStartup/src/com/rock/automation/Main.java
+++ CeremonialAssemblyLineStartup/src/com/rock/automation/Main.java
@@ -10,27 +10,27 @@ public class Main {
         int employeesNumber = 1100;
         int runModeCounter = 0;
         int programModeCounter = 0;
-        int[] anArray;
-        anArray = new int[plcsNumber];
+        int[] plcs;
+        plcs = new int[plcsNumber];
 
         for(int i=1;i<=employeesNumber;i++)
         {
             if (i==1) {
                 for(int j=1;j<=plcsNumber;j++) {
-                    anArray[j-1]=0;
+                    plcs[j-1]=0;
                 }
             } else {
                 for(int j=1;j<=plcsNumber;j=j+i) {
-                    if (anArray[j-1]==0) anArray[j-1]=1;
-                    else if (anArray[j-1]==1) anArray[j-1]=0;
+                    if (plcs[j-1]==0) plcs[j-1]=1;
+                    else if (plcs[j-1]==1) plcs[j-1]=0;
                 }
             }
         }
 
         for(int i=1;i<=1100;i++) {
-            if (anArray[i-1]==0) runModeCounter++;
+            if (plcs[i-1]==0) runModeCounter++;
             else programModeCounter++;
-            System.out.println("Element at index: " + i + " is " + anArray[i-1]);
+            System.out.println("Element at index: " + i + " is " + plcs[i-1]);
         }
         System.out.println("There are " + runModeCounter + " PLCs in RUN mode.");
         System.out.println("There are " + programModeCounter + " PLCs in PROGRAM mode.");
